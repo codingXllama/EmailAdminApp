a8bb0336cafac9825b7098639d32366f35b1efb7
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-06-30 21:26:23 -0600

    Created a package to store the Email application

---
+++ AdminApp/src/EmailApp/EmailAppRunner.java
@@ -0,0 +1,9 @@
+package EmailApp;
+
+public class EmailAppRunner {
+
+    public static  void main(String[] args)
+    {
+        EmailCore firstEmail= new EmailCore("Nuca","Nuci");
+    }
+}
---
+++ AdminApp/src/EmailApp/EmailCore.java
@@ -0,0 +1,38 @@
+package EmailApp;
+
+public class EmailCore {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int mailBoxCapacity;
+    private String alt_Email;
+
+
+
+    //1.Building the constructor to receive the first name and last name from the user
+
+    public EmailCore(String firstName, String lastName)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+    
+
+    //2.Asking for the department
+
+
+    //3. Generating a random password
+
+    //4. Setting the mailbox capacity
+
+
+    //5. Setting the alternative email
+
+
+    //6. Changing the password
+
+
+
+}
