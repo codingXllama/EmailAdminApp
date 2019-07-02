0d581e02320a2033f6739f772a972f7224c71b3c
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-07-01 22:55:33 -0600

    Created a showInfo method and fixed some text layout

--- AdminApp/src/EmailApp/EmailAppRunner.java
+++ AdminApp/src/EmailApp/EmailAppRunner.java
@@ -1,14 +1,21 @@
 package EmailApp;
 
+import java.util.Scanner;
+
 public class EmailAppRunner {
 
     public static  void main(String[] args)
     {
         EmailCore firstEmail= new EmailCore("Nuca","Nuci");
-        firstEmail.setAltEmail("zax@gmail.com");
-        firstEmail.SetEmailCapacity(21);
-        System.out.println(firstEmail.getAltEmail());
-        System.out.println(firstEmail.getMailBoxCapacity());
+        Scanner userInput =  new Scanner(System.in);
+        System.out.print("Enter alt email: ");
+        String altEmail=userInput.next() +".com";
+        firstEmail.setAltEmail(altEmail);
+
+        firstEmail.SetEmailCapacity(firstEmail.getMailBoxCapacity());
+       // System.out.println(firstEmail.getAltEmail());
+        //System.out.println(firstEmail.getMailBoxCapacity());
+        System.out.println(firstEmail.showAccountInfo());
     }
 
 }
--- AdminApp/src/EmailApp/EmailCore.java
+++ AdminApp/src/EmailApp/EmailCore.java
@@ -1,4 +1,5 @@
 package EmailApp;
+
 import java.util.*;
 
 public class EmailCore {
@@ -14,54 +15,55 @@ public class EmailCore {
     private String companySuffix;
 
 
-
     //1.Building the constructor to receive the first name and last name from the user
 
-    public EmailCore(String firstName, String lastName)
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
+    public EmailCore(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         //System.out.println("YAY! YOU HAVE CREATED YOUR EMAIL! "+firstName+" "+lastName);
 
         //After user enters their first and last name, we want them to enter their department
         //to do this we will call the setDepartment()
-        this.department=setDepartment();
-        System.out.println("Your Department is: "+department);
+        this.department = setDepartment();
+        //System.out.println("Your Department is: " + department);
 
 
         //After we make the user give us what department they are in
         //we will generate a random password for the user and store in the instance variable
         //before that we must get the random Generated password length
-        this.passwordLength=GetPasswordLength();
-        this.password=GenerateRandomPassword(passwordLength);
+        this.passwordLength = GetPasswordLength();
+        this.password = GenerateRandomPassword(passwordLength);
         //System.out.println("Your random generated password is: "+password);
 
         //Allowing the Admin to enter their company suffix
-        this.companySuffix=GetCompanySuffix();
+        this.companySuffix = GetCompanySuffix();
         //After we get the company suffix we will combine all the fName and Lname and companySuffix to form a single email
-        email=firstName.toLowerCase()+"."+lastName.toLowerCase()+"@"+department.toLowerCase()+"."+companySuffix.toLowerCase();
-        System.out.println("The company email is: "+email);
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department.toLowerCase() + "." + companySuffix.toLowerCase();
+        //System.out.println("The company email is: " + email);
 
         //Getting the email capacity
-//        this.mailBoxCapacity=GetEmailCapacity();
-        System.out.println("The email capacity is: "+mailBoxCapacity);
+          this.mailBoxCapacity=GetEmailCapacity();
+       // System.out.println("The email capacity is: " + mailBoxCapacity);
     }
 
 
     //2.Asking for the department
-    private String setDepartment()
-    {
+    private String setDepartment() {
         //creating user prompt
-        System.out.print("DEPARTMENT CODE #\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None\nENTER YOUR CODE #: ");
-        Scanner userInput=  new Scanner(System.in);
-        int userDepartment_Selection= userInput.nextInt();
+        System.out.print("DEPARTMENT CODE #\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None\n\nENTER YOUR CODE #: ");
+        Scanner userInput = new Scanner(System.in);
+        int userDepartment_Selection = userInput.nextInt();
 
         //checking the value for userDepartment_Selection
-        switch (userDepartment_Selection){
-            case 0: return "NONE"; //if the userDepartment Selection is 0
-            case 1: return "Sales";
-            case 2: return "Development";
-            case 3: return "Accounting";
+        switch (userDepartment_Selection) {
+            case 0:
+                return "NONE"; //if the userDepartment Selection is 0
+            case 1:
+                return "Sales";
+            case 2:
+                return "Development";
+            case 3:
+                return "Accounting";
             default:
                 System.out.println("Invalid input, please try again!");
         }
@@ -70,34 +72,29 @@ public class EmailCore {
 
 
     //3a. prompting user to give us a password length
-    private int GetPasswordLength()
-    {
+    private int GetPasswordLength() {
         System.out.print("Enter a # for how long you want the password to be: ");
-        Scanner userInput= new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         //returning the password length from the user
         return userInput.nextInt();
     }
 
 
-
-
     //3b. Generating a random password
-    private String GenerateRandomPassword(int passwordLength)
-    {
+    private String GenerateRandomPassword(int passwordLength) {
         //This string variable will store the possible characters that can make up a password
         //This variable acts like an array, we will just select select a random character from it and store it in the char[] password variable
-        String password_Possible_Characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        String password_Possible_Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
         //this char[] array will store the random generated password up to the size of the passwordLength that is passed in by this method
-        char[] randomGeneratedPassword= new char[passwordLength];
+        char[] randomGeneratedPassword = new char[passwordLength];
 
         //Generating a random password
-        for(int index=0; index<passwordLength;index++)
-        {
+        for (int index = 0; index < passwordLength; index++) {
             //generating a random index from 0 to the size of the password_Possible_Characters
-            int randomIndex = (int)(Math.random()*password_Possible_Characters.length());
+            int randomIndex = (int) (Math.random() * password_Possible_Characters.length());
 
             //Selecting a random character from the possible pw characters and storing it into the randomGeneratedPassword char variable
-            randomGeneratedPassword[index]=password_Possible_Characters.charAt(randomIndex);
+            randomGeneratedPassword[index] = password_Possible_Characters.charAt(randomIndex);
         }
         //converting a char array variable into a String using the `new String` keyword
         return new String(randomGeneratedPassword);
@@ -106,51 +103,54 @@ public class EmailCore {
 
 
     //3c. Allowing the user to enter their company suffix
-    private String GetCompanySuffix()
-    {
+    private String GetCompanySuffix() {
         System.out.print("Enter the company suffix: ");
-        Scanner userInput=  new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
-        return userInput.next()+".com";
+        return userInput.next() + ".com";
     }
 
 
 //    4a. Creating the method to set the email capacity from the Admin
-//    private int GetEmailCapacity()
-//    {
-//        System.out.print("Enter the Email Capacity: ");
-//        Scanner userInput = new Scanner(System.in);
-//        return userInput.nextInt();
-//    }
+    public int GetEmailCapacity()
+    {
+        System.out.print("Enter the Email Capacity: ");
+        Scanner userInput = new Scanner(System.in);
+        return userInput.nextInt();
+    }
 
     //4. Setting the mailbox capacity
     //creating the setter method which returns void and take a param.
-    public void SetEmailCapacity(int mailSize)
-    {
-        this.mailBoxCapacity=mailSize;
+    public void SetEmailCapacity(int mailSize) {
+        this.mailBoxCapacity = mailSize;
     }
 
     //5. Creating the setter for setting the alternative email
-    public void setAltEmail(String altEmail)
-    {
-        this.alt_Email=altEmail;
+    public void setAltEmail(String altEmail) {
+        this.alt_Email = altEmail;
     }
 
     //6. Changing the password
-    public void ChangePassword(String passwordToChange)
-    {
-        this.password=passwordToChange;
+    public void ChangePassword(String passwordToChange) {
+        this.password = passwordToChange;
     }
 
     //Creating getter for returning the mailBox capacity
-    public int getMailBoxCapacity()
-    {
+    public int getMailBoxCapacity() {
         return mailBoxCapacity;
     }
 
-    public String getAltEmail()
-    {
+    public String getAltEmail() {
         return alt_Email;
+    }
+
+    public String showAccountInfo() {
+        System.out.println("\n_________________________");
+        return "DISPLAY NAME: " + firstName + " " + lastName + "\n"+
+                "COMPANY EMAIL: " + email + "\n"+
+                "MAILBOX CAPACITY: " + mailBoxCapacity + "MB"+
+                "\n_________________________\n";
+
     }
 
 }
